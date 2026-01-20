dc1497a4cb9fa3eccb5758832f19834f45a938f7
Peter Heinrich <heip@zhaw.ch>
2026-01-20 15:50:35 +0100

    Committing failing test

--- src/test/java/com/example/demo/DemoApplicationTest.java
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,5 +9,6 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1,2);
     }
 }
